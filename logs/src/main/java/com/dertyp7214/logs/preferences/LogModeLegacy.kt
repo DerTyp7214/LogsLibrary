6bf84e22fcf497a0b70c016edad782e2287f803c
josua <josua.lengwenath@kickid.com>
2019-11-08 13:51:05 +0100

    Legacy style preference

---
+++ logs/src/main/java/com/dertyp7214/logs/preferences/LogModeLegacy.kt
@@ -0,0 +1,47 @@
+package com.dertyp7214.logs.preferences
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.preference.PreferenceManager
+import android.util.AttributeSet
+import androidx.preference.DropDownPreference
+import com.dertyp7214.logs.helpers.Logger
+import com.dertyp7214.preferencesplus.preferences.BottomSheetPreference
+
+class LogModeLegacy : DropDownPreference {
+    @SuppressLint("NewApi")
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(
+        context,
+        attrs,
+        defStyleAttr,
+        defStyleRes
+    ) {
+        init(attrs)
+    }
+
+    @SuppressLint("NewApi")
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs)
+    }
+
+    constructor(context: Context) : super(context) {
+        init(null)
+    }
+
+    private fun init(attrs: AttributeSet?) {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(Logger.context)
+        setDefaultValue("VERBOSE")
+        entries = arrayOf("VERBOSE", "ASSERT", "INFO", "WARN", "DEBUG", "ERROR", "CRASH")
+        entryValues = arrayOf("VERBOSE", "ASSERT", "INFO", "WARN", "DEBUG", "ERROR", "CRASH")
+        key = "logMode"
+        title = "Log-mode (${preferences.getString(key, "VERBOSE")})"
+        setOnPreferenceChangeListener { _, newValue ->
+            title = "Log-mode ($newValue)"
+            true
+        }
+    }
+}
